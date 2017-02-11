b36cfb57e743816d7367e25870349c2adcbb99ac
Aleš Holubec <ales@holubec.net>
2017-02-11 23:10:50 +0100

    Oprava souradnic

--- libs/googlemaps.js
+++ libs/googlemaps.js
@@ -9,7 +9,7 @@
                     zoom: 15,
 
                     // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(50.0532713, 14.5140896), // Kamelova 3
+                    center: new google.maps.LatLng(50.053251, 14.514284), // Kamelova 3
                     // center: new google.maps.LatLng(50.0795239, 14.4302956), // Opletalova
 
                     // How you would like to style the map. 
